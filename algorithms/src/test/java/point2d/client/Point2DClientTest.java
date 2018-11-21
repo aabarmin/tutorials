14b60cd25ff6568bb81d4fa0a49c7f81a5f2b5b5
Aleksandr Barmin <Aleksandr_Barmin@epam.com>
2018-11-21 13:04:22 +0300

    Simple algorithm from book was implemented

---
+++ algorithms/src/test/java/point2d/client/Point2DClientTest.java
@@ -0,0 +1,26 @@
+package point2d.client;
+
+import edu.princeton.cs.algs4.Point2D;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Point2DClientTest {
+    @Test
+    void testWithOverlappingPoints() {
+        assertEquals(0, Point2DClient.getDistance(new Point2D[]{
+                new Point2D(0, 0),
+                new Point2D(0, 0)
+        }));
+    }
+
+    @Test
+    void testWithDistance() {
+        Point2D[] points = new Point2D[] {
+                new Point2D(0, 0),
+                new Point2D(0, 2),
+                new Point2D(0, 3)
+        };
+        assertEquals(1, Point2DClient.getDistance(points));
+    }
+}
