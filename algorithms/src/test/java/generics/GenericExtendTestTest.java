e81c845cd6cbd310fcbdc77f8d03d5b00460252b
Aleksandr Barmin <Aleksandr_Barmin@epam.com>
2018-11-30 17:26:56 +0300

    Generics test

---
+++ algorithms/src/test/java/generics/GenericExtendTestTest.java
@@ -0,0 +1,18 @@
+package generics;
+
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Type;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GenericExtendTestTest {
+    @Test
+    void defaultTest() throws Exception {
+        final GenericExtendTest<Integer> integerGenericTest = new GenericExtendTest<>();
+        assertArrayEquals(new Type[0], integerGenericTest.getClass().getGenericInterfaces());
+        assertEquals(Object.class, integerGenericTest.getClass().getGenericSuperclass());
+        assertEquals(Number.class, integerGenericTest.getClass().getDeclaredField("value").getType());
+        assertEquals(Number.class, integerGenericTest.getClass().getDeclaredMethod("getValue").getReturnType());
+    }
+}
