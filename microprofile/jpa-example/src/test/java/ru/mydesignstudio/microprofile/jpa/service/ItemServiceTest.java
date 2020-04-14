f3ca9d67907b5d3fbaed4e2321a1c837dd4d67a0
Aleksandr Barmin <barmin.alexander@gmail.com>
2020-04-14 21:53:59 +0300

    Adding a Quarkus example to work with JPA

---
+++ microprofile/jpa-example/src/test/java/ru/mydesignstudio/microprofile/jpa/service/ItemServiceTest.java
@@ -0,0 +1,29 @@
+package ru.mydesignstudio.microprofile.jpa.service;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+import ru.mydesignstudio.microprofile.jpa.entity.ItemEntity;
+
+import javax.inject.Inject;
+import java.util.Collection;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@QuarkusTest
+class ItemServiceTest {
+  @Inject ItemService itemService;
+
+  @Test
+  void check_contextStarts() {
+    assertNotNull(itemService);
+  }
+
+  @Test
+  void findAll_itemsExist() {
+    final Collection<ItemEntity> items = itemService.findAll();
+
+    assertNotNull(items);
+    assertTrue(items.size() > 0);
+  }
+}
