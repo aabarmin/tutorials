353bdfda63b6defb549f4df9732606d42915a784
Aleksandr Barmin <barmin.alexander@gmail.com>
2020-04-19 18:54:00 +0300

    test(spring-test): fixing tests that are using in-memory H2 database
    Due to the programmatic error the @Sql annotation wasn't working, the @Transactional annotation was missed. Due to it the database wasn't populate that lead to the test error.

--- spring/spring-test/src/test/java/ru/mydesignstudio/spring/test/PersonControllerTest.java
+++ spring/spring-test/src/test/java/ru/mydesignstudio/spring/test/PersonControllerTest.java
@@ -6,15 +6,15 @@ import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 import ru.mydesignstudio.spring.test.web.WebConfiguration;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@Transactional
 @SpringJUnitWebConfig(WebConfiguration.class)
 @Sql("classpath:create-tables.sql")
 public class PersonControllerTest {
--- spring/spring-test/src/test/java/ru/mydesignstudio/spring/test/TransactionalTest.java
+++ spring/spring-test/src/test/java/ru/mydesignstudio/spring/test/TransactionalTest.java
@@ -3,13 +3,15 @@ package ru.mydesignstudio.spring.test;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@SpringJUnitConfig(InMemoryDataSourceConfiguration.class)
 @Transactional
+@Sql("classpath:create-tables.sql")
+@SpringJUnitConfig(InMemoryDataSourceConfiguration.class)
 public class TransactionalTest {
     @Autowired
     private JdbcTemplate jdbcTemplate;
